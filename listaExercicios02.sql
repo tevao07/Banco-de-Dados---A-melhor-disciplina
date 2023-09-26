f98eb019eb67b79148c71156a5cda9210478ba15
Tevin Soca Fofo <115832908+tevao07@users.noreply.github.com>
2023-09-26 20:37:45 -0300

    commit 10

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -156,3 +156,19 @@ BEGIN
 
 
 DELIMITER 
+
+
+10. Livros e Seus Autores:
+
+sql
+DELIMITER //
+
+CREATE PROCEDURE sp_LivrosESeusAutores()
+BEGIN
+    SELECT Livro.Titulo, CONCAT(Autor.Nome, ' ', Autor.Sobrenome) AS 'Autor'
+    FROM Livro
+    INNER JOIN Autor_Livro ON Livro.Livro_ID = Autor_Livro.Livro_ID
+    INNER JOIN Autor ON Autor_Livro.Autor_ID = Autor.Autor_ID;
+
+
+DELIMITER ;
