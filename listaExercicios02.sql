14916c248ce0f0a28fd128713b9473ef20dfa9af
Tevin Soca Fofo <115832908+tevao07@users.noreply.github.com>
2023-09-26 20:34:16 -0300

    commit 4

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -41,3 +41,24 @@ BEGIN
 
 DELIMITER ;
 
+4. Verificação de Livros por Categoria:
+
+sql
+DELIMITER //
+
+CREATE PROCEDURE sp_VerificarLivrosCategoria(IN categoriaNome VARCHAR(100), OUT possuiLivros BOOLEAN)
+BEGIN
+    DECLARE total INT;
+    SELECT COUNT(*) INTO total
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+
+    IF total > 0 THEN
+        SET possuiLivros = TRUE;
+    ELSE
+        SET possuiLivros = FALSE;
+    END IF;
+
+
+DELIMITER ;
