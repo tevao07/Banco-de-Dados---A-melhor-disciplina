29e607d180bdf2e3ebfde2ed1e2815fe6148f1c4
Tevin Soca Fofo <115832908+tevao07@users.noreply.github.com>
2023-09-26 20:33:23 -0300

    commit 2

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -9,3 +9,19 @@ BEGIN
 
 
 DELIMITER ;
+
+
+2. Livros por Categoria:
+
+sql
+DELIMITER //
+
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoriaNome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+
+
+DELIMITER ;
